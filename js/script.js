7ded902c4ac9873772d34d69311f5189d958a956
KimVictoria <kimberlyvmagpantay@icloud.com>
2019-10-01 01:30:08 +0000

    Started the project. wrote some JS :))

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $( "document" ).ready(function() {
+    console.log("Hello World!")
+$('#button').click(function() {
+    var test =
+    $('.input').val();
+    $('.output').html(test);
+});
 
-
+function pig (test) {
+    var firstletter = word[0]
+    return firstletter + "ay";
+}
 
 });
